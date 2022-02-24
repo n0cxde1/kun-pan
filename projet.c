dbb2eed37b1b6aaeb2e79819435085e72fba1139
Claudel N'DAH <n4dah2claudel518@gmail.com>
2022-02-24 17:32:14 +0100

    voici mon nouveau projet

---
+++ projet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+   char reponse[10];
+   int i = 1;
+
+   while(i < 10)
+   {
+    printf("As tu manger aujourd'hui ? \n");
+    scanf("%s", reponse);
+    if(reponse[0] == 'a' || reponse[0] == 'A')
+        printf("Bonne digestion \n");
+    else if(reponse[0] == 'n' || reponse[0] == 'N')
+        printf("va manger \n");
+    i++;
+   }
